d93fc33004e1ce7fbbe0da16fda68a24ee74ec5c
Bhanu <bhanupratapsinghq@gmail.com>
2024-05-19 14:47:08 +0530

    constructor with exception handling.

---
+++ src/main/java/com/dsa2024/OPPS/Constructor/Constructor_With_Exception/InvalidAgeException.java
@@ -0,0 +1,8 @@
+package com.dsa2024.OPPS.Constructor.Constructor_With_Exception;
+
+class InvalidAgeException extends Exception {
+    public InvalidAgeException(String message) {
+        super(message);
+    }
+}
+
