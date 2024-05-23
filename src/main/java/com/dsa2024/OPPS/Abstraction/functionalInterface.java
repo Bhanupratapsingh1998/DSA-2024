e80c66f13e3af56a23123e8cf9b37c699a3e4fdd
Bhanu <bhanupratapsinghq@gmail.com>
2024-05-24 00:52:15 +0530

    Functional interface.

---
+++ src/main/java/com/dsa2024/OPPS/Abstraction/functionalInterface.java
@@ -0,0 +1,16 @@
+package com.dsa2024.OPPS.Abstraction;
+@FunctionalInterface
+interface Calculator {
+    int calculate(int a, int b);
+}
+
+public class functionalInterface {
+    public static void main(String[] args) {
+        // Using lambda expression
+        Calculator addition = (a, b) -> a + b;
+        Calculator multiplication = (a, b) -> a * b;
+
+        System.out.println("Addition: " + addition.calculate(5, 3));  
+        System.out.println("Multiplication: " + multiplication.calculate(5, 3));  
+    }
+}
