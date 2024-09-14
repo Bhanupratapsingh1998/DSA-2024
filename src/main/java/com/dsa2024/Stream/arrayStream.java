525b7ad205b3aa4a1271f6966eadcefde8c5c6fd
Bhanu <bhanupratapsinghq@gmail.com>
2024-09-14 16:31:57 +0530

    Getting second highest salary using in stream

--- src/main/java/com/dsa2024/Stream/arrayStream.java
+++ src/main/java/com/dsa2024/Stream/arrayStream.java
@@ -1,6 +1,7 @@
 package com.dsa2024.Stream;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.stream.Stream;
 
 public class arrayStream {
@@ -8,5 +9,9 @@ public class arrayStream {
         Integer num[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
         Stream<Integer> stream = Arrays.stream(num);
         stream.filter(e -> e > 5).forEach(e -> System.out.println(e));
+
+        // Getting second highest salary using in stream
+        Integer a = Stream.of(num).sorted(Comparator.reverseOrder()).skip(1).findFirst().get();
+        System.out.println("Secod highest no is " + a);
     }
 }
