c19df2128e934be4a2bc6796e54eb4c12710e65f
Bhanu <bhanupratapsinghq@gmail.com>
2024-11-17 22:53:18 +0530

    Hot fixed

--- src/main/java/com/dsa2024/LeetCode/removeDulpicateSorted.java
+++ src/main/java/com/dsa2024/LeetCode/removeDulpicateSorted.java
@@ -32,7 +32,8 @@ public class removeDulpicateSorted {
         int i = 0;
         for (int j = 1; j < arr.length; j++) {
             if (arr[i] != arr[j]) {
-                arr[i++] = arr[j];
+                i++;
+                arr[i] = arr[j];
             }
         }
         return i + 1;
@@ -40,8 +41,8 @@ public class removeDulpicateSorted {
 
     public static void main(String[] args) {
         int arr[] = { 1, 1, 2, 2, 2, 3, 3, 4, 5, 5, 5, 6, 6 };
-        // int k = removeDuplicates1(arr);
-        int k = removeDuplicates(arr);
+        int k = removeDuplicates1(arr);
+        // int k = removeDuplicates(arr);
         System.out.println("The array after removing duplicate elements is ");
         for (int i = 0; i < k; i++) {
             System.out.print(arr[i] + " ");
