c367b7b0e656eaab58c4799df8822d65647ef55d
Bhanu <bhanupratapsinghq@gmail.com>
2024-11-17 22:51:44 +0530

    Fixed the duplicate element issue

--- src/main/java/com/dsa2024/LeetCode/removeDulpicateSorted.java
+++ src/main/java/com/dsa2024/LeetCode/removeDulpicateSorted.java
@@ -32,15 +32,14 @@ public class removeDulpicateSorted {
         int i = 0;
         for (int j = 1; j < arr.length; j++) {
             if (arr[i] != arr[j]) {
-                i++;
-                arr[i] = arr[j];
+                arr[i++] = arr[j];
             }
         }
         return i + 1;
     }
 
     public static void main(String[] args) {
-        int arr[] = { 1, 1, 2, 2, 2, 3, 3 };
+        int arr[] = { 1, 1, 2, 2, 2, 3, 3, 4, 5, 5, 5, 6, 6 };
         // int k = removeDuplicates1(arr);
         int k = removeDuplicates(arr);
         System.out.println("The array after removing duplicate elements is ");
