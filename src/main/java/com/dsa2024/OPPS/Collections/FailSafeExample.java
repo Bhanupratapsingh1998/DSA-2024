4a188bc7b76c92383ff75aa12d9eb35480c5a368
Bhanu <bhanu.pratap@hashstudioz.com>
2024-12-23 12:47:12 +0530

    Fail-safe and Fail-fast example

---
+++ src/main/java/com/dsa2024/OPPS/Collections/FailSafeExample.java
@@ -0,0 +1,21 @@
+package com.dsa2024.OPPS.Collections;
+
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class FailSafeExample {
+    public static void main(String[] args) {
+        CopyOnWriteArrayList<String> list = new CopyOnWriteArrayList<>();
+        list.add("A");
+        list.add("B");
+        list.add("C");
+
+        for (String item : list) {
+            System.out.println(item);
+            // Structural modification: adding an element while iterating
+            list.add("D");
+        }
+
+        System.out.println("Final list: " + list);
+    }
+}
+
